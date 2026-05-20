7e271539bb8c6d3e87763cad2d4e375c364fef3e
Official-Vinit <officialvinit1176@gmail.com>
2026-05-20 23:19:53 +0530

    add: Navbar

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Nav from '../components/nav'
 
 function Home() {
   return (
-    <div className='text-2xl text-[red]'>home page</div>
+    <div className='w-[100%] overflow-hidden'>
+
+      <div className='w-[100%] lg:h-[140vh] h-[70vh] relative'>
+        <Nav />
+      </div>
+    </div>
   )
 }
 
