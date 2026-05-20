8465a33ea8b922d0af68b2b58ca5cb03637f6d8f
Official-Vinit <officialvinit1176@gmail.com>
2026-05-20 15:30:27 +0530

    add: auth and get user with redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,16 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
+import useCurrentUser from './customHooks/useCurrentUser'
+import { ToastContainer } from 'react-toastify'
 
 export const serverUrl = 'http://localhost:8000';
 
 function App() {
+  useCurrentUser()
   return (
     <>
+      <ToastContainer />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/login' element={<Login/>} />
